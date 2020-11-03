0c5241d78ae78172c7b574884d27cc658f7f1ee4
syipmanl <stanley3558@Hotmail.com>
2020-11-03 11:31:03 -0500

    STATIC ROOT 4th  Try

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -125,7 +125,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'store/static'),
 ]
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'store/static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/store/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'store/static/store/images')
