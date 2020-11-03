7a89bcc69e851604cfee3447f9773e5f72087826
syipmanl <stanley3558@Hotmail.com>
2020-11-03 12:11:39 -0500

    Updated Settings, Secret Key and Email/Pass

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '-p1h_tz!i73++hz$ura2d#+f&0xgeirpog68kx^-zqx*_a1!&v'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
